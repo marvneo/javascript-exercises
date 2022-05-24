a5aa87773b83a48550cef1025672b1a8ef5a9b01
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 03:35:21 -0400

    add solution for excercise 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    argument = [...args]
+    count = argument.length
+    array = argument[0]
+
+    for ( let i = count - 1 ; i >= 1; i-- ) {
+
+        toSplice = argument[i]
+
+        indexToSplice = array.indexOf(toSplice)
+
+        if (indexToSplice >= 0) {
+            array.splice(indexToSplice,1)
+        }
+        
+
+    }
+    return array
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
