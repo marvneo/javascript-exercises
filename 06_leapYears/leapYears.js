ac97c352fdec08de47126a23194d8338a49a4db8
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 12:51:20 -0400

    add solution for 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    isDivisibleByFour = Number.isInteger(year/4)
+    isDivisibleByFourHunnit = true
+
+    if (Number.isInteger(year/100)) {
+        isDivisibleByFourHunnit = Number.isInteger(year/400)
+    }
+    
+
+    return isDivisibleByFour && isDivisibleByFourHunnit
 };
 
 // Do not edit below this line
