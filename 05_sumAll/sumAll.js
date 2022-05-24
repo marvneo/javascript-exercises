0071db9c8217ead83dacac3bfd72745a8bac2346
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 12:40:45 -0400

    add solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (num1 > 0 && num2 > 0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+        maxNum = Math.max(num1,num2)
+        minNum = Math.min(num1,num2)
+
+        difference =  maxNum - minNum
+
+        output = num1 + num2
+
+        for ( i = minNum + 1 ; i < maxNum; i++ ) {
+            output += i
+        }
+
+        return output
+    } else {
+        return 'ERROR'
+    }
+
+    
 
 };
 
