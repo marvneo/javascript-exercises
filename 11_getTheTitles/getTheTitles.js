416805383d10181ff7cf796098387f8a0dfd4cfa
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 23:23:10 -0400

    add solutiont to 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
 
+    output = []
+
+    for (const book of bookArray) {
+        output.push(book.title)
+    }
+
+    return output
 };
 
 // Do not edit below this line
