a81d8c90067eff726ba341dae21fce781f9b8ebe
Marvin Neoh <neoh@ualberta.ca>
2022-05-25 02:21:50 -0400

    add solution to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    maxSoFar = 0
+
+    mausoleom = {}
+
+    for (person of array) {
+
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth
+        } else {
+            age = 2022 -person.yearOfBirth
+        }
+
+        mausoleom[age] = person
+        maxSoFar = Math.max(maxSoFar, age)
+    }
+
+    return mausoleom[maxSoFar]
 };
 
 // Do not edit below this line
