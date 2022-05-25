37ef6715a2a45056e8e201d71f119f145a7ade6a
Marvin Neoh <neoh@ualberta.ca>
2022-05-25 03:03:37 -0400

    add solution to 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,47 @@
-const caesar = function() {
+const caesar = function(message, shifts) {
 
+    output = ''
+
+    // Making sure shifts remain in range
+    shifts = shifts % 26
+
+    // Acess each character
+    for (i = 0; i < message.length; i++) {
+        
+        oriCode = message.charCodeAt(i)
+        shiftedCode = oriCode + shifts
+
+        // Checks for lower case letters
+        if (oriCode > 96 && oriCode < 123) {
+          
+            if (shiftedCode > 122) {
+                shiftedCode = (shiftedCode % 122) + 96
+            } else if (shiftedCode < 97) {
+                shiftedCode = 123 - (97 % shiftedCode)
+            }
+            
+            output += String.fromCharCode(shiftedCode)
+        
+
+        // Checks for upper case letters
+        } else if (oriCode > 64 && oriCode < 91) {
+
+            if (shiftedCode > 90) {
+                shiftedCode = (shiftedCode % 90) + 64
+            } else if (shiftedCode < 65) {
+                shiftedCode = 91 - (65 % shiftedCode)
+            }
+
+            output += String.fromCharCode(shiftedCode)
+
+        } else {
+
+            output += message[i]
+        
+        }
+
+    }
+    return output
 };
 
 // Do not edit below this line
