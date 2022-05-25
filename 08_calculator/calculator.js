998dfd59e2febf8848ec2a5acb2d3e88df5df02f
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 22:25:52 -0400

    add solution for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
-	
-};
+const add = function(a, b) {
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return (a+b)
 
 };
 
-const power = function() {
+const subtract = function(a,b) {
 	
+  return (a-b)
+
 };
 
-const factorial = function() {
+const sum = function(array) {
 	
+  output = 0
+
+  for (i = 0; i < array.length; i++) {
+
+    output += array[i]
+
+  }
+
+  return output
+
+};
+
+const multiply = function(array) {
+  
+  output = 1
+  
+  for (i = 0; i < array.length; i ++) {
+    output = output * array[i]
+  }
+
+  return output
+
+};
+
+const power = function(a,b) {
+	return a**b
+};
+
+const factorial = function(num) {
+  
+  if (num <= 0) {
+    return 1
+  } else {
+    output = 1
+    i = num
+    while (i > 0) {
+      output = output * i
+      i --
+  }
+
+  return output
+  }
+  
 };
 
 // Do not edit below this line
