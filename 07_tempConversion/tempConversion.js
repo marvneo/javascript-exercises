ec8bbbab181f88e303a52e4da1fe4a428290d04b
Marvin Neoh <neoh@ualberta.ca>
2022-05-24 21:58:33 -0400

    add solution 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  
+  output = ((tempF - 32) * .5556)
 
+  if (! Number.isInteger(output)) {
+    output = Number(output.toFixed(1));
+  }
+
+  return output 
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
 
+  output = ((tempC * 1.8) + 32)
+
+  if (! Number.isInteger(output)) {
+    output = Number(output.toFixed(1))
+  }
+
+
+  return output
 };
 
 // Do not edit below this line
